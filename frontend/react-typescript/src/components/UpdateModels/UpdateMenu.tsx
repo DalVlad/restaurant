5c018e006a50b2ed2f9616b8da563f4cbfd37ff4
Dalv <vladbulanov47@gmail.com>
2023-02-13 15:13:19 +0300

    adding and removing dishes

--- frontend/react-typescript/src/components/UpdateModels/UpdateMenu.tsx
+++ frontend/react-typescript/src/components/UpdateModels/UpdateMenu.tsx
@@ -1,8 +1,8 @@
 import "../style/forma.css";
 import { useQuery } from "react-query";
-import { Formik } from "formik";
+import { FieldArray, Formik } from "formik";
 import { dishModel } from "../Models/dishModel";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import { AxiosError } from "axios";
 import HttpService from "../HttpService";
 import { menuModel } from "../Models/menuModel";
@@ -14,6 +14,7 @@ const UpdateDish = () => {
     ["menu", id],
     () => HttpService.getMenu(idMenu)
   );
+  const navigator = useNavigate();
   const dishAll = useQuery<dishModel[], AxiosError>(
     ["dishes"],
     HttpService.getAllDish
@@ -21,7 +22,7 @@ const UpdateDish = () => {
   return (
     <div className="updateMenu">
       <div className="forma">
-        {isLoading ? (
+        {isLoading || dishAll.isLoading ? (
           <div>IsLoading</div>
         ) : (
           <Formik
@@ -33,63 +34,97 @@ const UpdateDish = () => {
               dishes: data!.dishes,
             }}
             onSubmit={(values) => {
-              HttpService.updateMenu(values.id, values);
+              HttpService.updateMenu(values.id, values).then(() =>
+                navigator("/menus")
+              );
             }}
           >
             {(props) => (
               <div className="allDivInMenu">
                 <div className="formMenu">
-                    <form onSubmit={props.handleSubmit}>
+                  <form onSubmit={props.handleSubmit}>
                     <div>
-                        <label htmlFor="name">Название меню</label>
-                        <input
+                      <label htmlFor="name">Название меню</label>
+                      <input
                         type="text"
                         onChange={props.handleChange}
                         value={props.values.name}
                         name="name"
-                        />
+                      />
                     </div>
                     <div>
-                        <label htmlFor="timeStart">С</label>
-                        <input
+                      <label htmlFor="timeStart">С</label>
+                      <input
                         type="time"
                         onChange={props.handleChange}
                         value={props.values.timeStart}
                         name="timeStart"
-                        />
+                      />
                     </div>
                     <div>
-                        <label htmlFor="timeEnd">До</label>
-                        <input
+                      <label htmlFor="timeEnd">До</label>
+                      <input
                         type="time"
                         onChange={props.handleChange}
                         value={props.values.timeEnd}
                         name="timeEnd"
-                        />
+                      />
                     </div>
                     <div>
+                      <FieldArray name={"dishes"}>
+                        {(arrayHelpers) => (
+                          <div>
+                            <button
+                              onClick={() => arrayHelpers.push({name: " ", id: 0 } as dishModel)}
+                              type="button"
+                            >
+                              Добавить блюдо
+                            </button>
+                            <table>
+                              <thead>
+                                <tr>
+                                  <th>Блюдо</th>
+                                </tr>
+                              </thead>
+                              <tbody>
+                                {props.values.dishes.map((el, index) => (
+                                  <tr>
+                                    <td>
+                                      <select
+                                        name={`dishes[${index}].id`}
+                                        onChange={props.handleChange}
+                                        defaultValue={el.id}
+                                      >
+                                        {dishAll.data?.map((el2) => (
+                                          <option key={el2.id} value={el2.id}>
+                                            {el2.name} ({el2.price})
+                                          </option>
+                                        ))}
+                                      </select>
+                                    </td>
+                                    <td>
+                                      <button
+                                        onClick={() =>
+                                          arrayHelpers.remove(index)
+                                        }
+                                        type="button"
+                                      >
+                                        Удалить
+                                      </button>
+                                    </td>
+                                  </tr>
+                                ))}
+                              </tbody>
+                            </table>
+                          </div>
+                        )}
+                      </FieldArray>
                     </div>
                     <button type="submit">Изменить</button>
-                    <button type="submit" onClick={() => props.values.id = 0}>Создать</button>
-                    </form>
-                    <div>
-                <label>В меню</label>
-                  <div className="inMenu">
-                    {props.values.dishes.map((el) => (
-                        <p key={el.id}>- {el.name} : {el.price} руб.</p>
-                    ))}
-                  </div>
-                  </div>
-                </div>
-                <div>
-                    <div>
-                    <label>Доступные блюда для добавления</label>
-                    {dishAll.data?.map((el) => (
-                    <div key={el.id}>
-                        <p>- {el.name} : {el.price} руб.</p> 
-                    </div>
-                    ))}
-                    </div>
+                    <button type="submit" onClick={() => (props.values.id = 0)}>
+                      Создать
+                    </button>
+                  </form>
                 </div>
               </div>
             )}
